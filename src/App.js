34d7c5681766e6c985d59a59569c27715415c86f
MartyTheeMartian <martytheemartian@yahoo.com>
2022-11-01 10:14:26 -0700

    Initial Setup: WebPack, Babel

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
